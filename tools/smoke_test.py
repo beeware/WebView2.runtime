40ba4ec0cbe0bed52ef33bf91b0d5099344e937d
Russell Keith-Magee <russell@keith-magee.com>
2026-07-24 13:53:48 +0800

    Fix pre-commit issues.

--- tools/smoke_test.py
+++ tools/smoke_test.py
@@ -5,11 +5,9 @@ from pythonnet import set_runtime
 
 # runtime.json defines the .NET version. .NET 10 is the current LTS release.
 set_runtime(
-    clr_loader.get_coreclr(
-        runtime_config=Path(__file__).parent / "runtime.json"
-    )
+    clr_loader.get_coreclr(runtime_config=Path(__file__).parent / "runtime.json")
 )
 
-import WebView2
+import WebView2  # noqa: E402
 
 print(f"{WebView2.__version__=}")
